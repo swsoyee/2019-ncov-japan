bbe6e9ada402d78dfa92c9a0c05fc04e22a58251
swsoyee <swsoyee@gmail.com>
2020-01-31 23:20:33 +0900

    [clean] コードフォーマットと文言の微修正

--- server.R
+++ server.R
@@ -29,7 +29,8 @@ shinyServer(function(input, output, session) {
     p <- plot_ly(
       x =  ~ xData,
       y = ~ yData$V1,
-      name = lang[[langCode]][4], # 全国
+      name = lang[[langCode]][4],
+      # 全国
       type = "scatter",
       mode = 'lines+markers'
     ) %>% layout(
@@ -49,41 +50,55 @@ shinyServer(function(input, output, session) {
     provinceHasDataset <- db[hasData]
     for (i in 1:nrow(provinceHasDataset)) {
       p <-
-        add_trace(p,
-                  x = xData,
-                  y = cumsum(transpose(provinceHasDataset[i, 2:ncol(provinceHasDataset)]))$V1,
-                  name = provinceHasDataset[i, 1],
-                  line = list(dash = 'dot'))
+        add_trace(
+          p,
+          x = xData,
+          y = cumsum(transpose(provinceHasDataset[i, 2:ncol(provinceHasDataset)]))$V1,
+          name = provinceHasDataset[i, 1],
+          line = list(dash = 'dot')
+        )
     }
     p
   })
   
   output$totalConfirmed <- renderValueBox({
-    valueBox(value = sum(db[, 2:ncol(db)]), 
-             subtitle = paste0(lang[[langCode]][9], '*'), # 確認数 
-             icon = icon('sad-tear'),
-             color = "red")
+    valueBox(
+      value = sum(db[, 2:ncol(db)]),
+      subtitle = paste0(lang[[langCode]][9], '*'),
+      # 確認数
+      icon = icon('sad-tear'),
+      color = "red"
+    )
   })
   
   output$totalSuspicious <- renderValueBox({
-    valueBox(value = "？", 
-             subtitle = lang[[langCode]][8], # 観察中 
-             icon = icon('flushed'),
-             color = "orange")
+    valueBox(
+      value = "？",
+      subtitle = lang[[langCode]][8],
+      # 観察中
+      icon = icon('flushed'),
+      color = "orange"
+    )
   })
   
   output$totalDeath <- renderValueBox({
-    valueBox(value = "0", 
-             subtitle = lang[[langCode]][7], # 死亡数 
-             icon = icon('dizzy'),
-             color = "navy")
+    valueBox(
+      value = "0",
+      subtitle = lang[[langCode]][7],
+      # 死亡数
+      icon = icon('dizzy'),
+      color = "navy"
+    )
   })
   
   output$totalRecovered <- renderValueBox({
-    valueBox(value = "1", 
-             subtitle = lang[[langCode]][6], # 完治数 
-             icon = icon('grin-squint'),
-             color = "green")
+    valueBox(
+      value = "1",
+      subtitle = lang[[langCode]][6],
+      # 完治数
+      icon = icon('grin-squint'),
+      color = "green"
+    )
   })
   
   output$totalConfirmedByProvince <- renderDataTable({
@@ -91,41 +106,51 @@ shinyServer(function(input, output, session) {
     tableDt <- data.table('行政区域' = db$name, # 行政区域
                           '確認数' = total # 確認数
                           )
-    # displayData <- tableDt[確認数 > 0][order(-確認数)]
-    displayData <- tableDt[order(-確認数)]
-    datatable(displayData,
-              rownames = FALSE,
-              options = list(dom = 't',
-                             scrollY = '440px',
-                             scrollCollapse = T)) %>%
-              formatStyle(c('確認数'),
-                          background = styleColorBar(range(tableDt[, '確認数']), 'lightblue'),
-                          backgroundSize = '98% 88%',
-                          backgroundRepeat = 'no-repeat',
-                          backgroundPosition = 'center')
+                          # displayData <- tableDt[確認数 > 0][order(-確認数)]
+                          displayData <- tableDt[order(-確認数)]
+                          datatable(
+                            displayData,
+                            rownames = FALSE,
+                            options = list(
+                              dom = 't',
+                              scrollY = '440px',
+                              scrollCollapse = T,
+                              paging = F
+                            )
+                          ) %>%
+                            formatStyle(
+                              c('確認数'),
+                              background = styleColorBar(range(tableDt[, '確認数']), 'lightblue'),
+                              backgroundSize = '98% 88%',
+                              backgroundRepeat = 'no-repeat',
+                              backgroundPosition = 'center'
+                            )
   })
-  
+    
   output$news <- renderDataTable({
-    newsData <-
-      data.table(paste0(
-        "<small>",
-        as.POSIXct(as.character(news$date), format = "%Y%m%d"),
-        "</small><br><a href='",
-        news$link,
-        "'>",
-        news$title,
-        "</a>"
-      ))
-    datatable(
-      newsData[order(-V1)],
-      options = list(
-        dom = 't',
-        scrollCollapse = T,
-        scrollY = '440px'
-      ),
-      rownames = FALSE,
-      colnames = lang[[langCode]][14], # 最新情報
-      escape = FALSE
-    )
-  })
+      newsData <-
+        data.table(
+          paste0(
+            "<small>",
+            as.POSIXct(as.character(news$date), format = "%Y%m%d"),
+            "</small><br><a href='",
+            news$link,
+            "'>",
+            news$title,
+            "</a>"
+          )
+        )
+      datatable(
+        newsData[order(-V1)],
+        options = list(
+          dom = 't',
+          scrollCollapse = T,
+          scrollY = '440px'
+        ),
+        rownames = FALSE,
+        colnames = lang[[langCode]][14],
+        # 最新情報
+        escape = FALSE
+      )
+    })
 })
--- ui.R
+++ ui.R
@@ -2,36 +2,39 @@ source(file = "global.R",
        local = TRUE,
        encoding = "UTF-8")
 
-shinyUI(dashboardPage(skin = "purple",
-  dashboardHeader(title = lang[[langCode]][1]), # 2019-nCov 日本
+shinyUI(dashboardPage(
+  skin = "purple",
+  dashboardHeader(title = lang[[langCode]][1]),
+  # 2019-nCov 日本
   dashboardSidebar(disable = T),
-  dashboardBody(fluidPage(fluidRow(
-    valueBoxOutput(width = 3, "totalConfirmed"),
-    valueBoxOutput(width = 3, "totalSuspicious"),
-    valueBoxOutput(width = 3, "totalDeath"),
-    valueBoxOutput(width = 3, "totalRecovered")
-  ),fluidRow(
-    box(
-      width = 8,
-      title = lang[[langCode]][2], # 新型コロナウイルス感染症マップ
-      plotOutput("map"),
-      footer = paste(lang[[langCode]][5] # ソース：厚生労働省
-                     , '(', UPDATE_TIME, ')')
+  dashboardBody(fluidPage(
+    fluidRow(
+      valueBoxOutput(width = 3, "totalConfirmed"),
+      valueBoxOutput(width = 3, "totalSuspicious"),
+      valueBoxOutput(width = 3, "totalDeath"),
+      valueBoxOutput(width = 3, "totalRecovered")
     ),
-    box(
-      width = 4,
-      dataTableOutput('totalConfirmedByProvince')
-    ),
-    box(
-      width = 8,
-      title = lang[[langCode]][3], # 確認累積数
-      plotlyOutput("confirmedAccumulation"),
-      # 無症状病原体保有者を含む
-      footer = paste('*', lang[[langCode]][12])
-    ),
-    box(
-      width = 4,
-      dataTableOutput('news')
+    fluidRow(
+      box(
+        width = 8,
+        title = lang[[langCode]][2],
+        # 新型コロナウイルス感染症マップ
+        plotOutput("map"),
+        footer = paste(lang[[langCode]][5] # ソース：厚生労働省
+                       , '(', UPDATE_TIME, ')')
+      ),
+      box(width = 4,
+          dataTableOutput('totalConfirmedByProvince')),
+      box(
+        width = 8,
+        title = lang[[langCode]][3],
+        # 確認累積数
+        plotlyOutput("confirmedAccumulation"),
+        # 無症状病原体保有者を含む
+        footer = paste('*', lang[[langCode]][12])
+      ),
+      box(width = 4,
+          dataTableOutput('news'))
     )
-  )))
+  ))
 ))
