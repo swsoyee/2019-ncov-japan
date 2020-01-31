70764876123c8f1e47467b8e154c4127a6ed28ce
swsoyee <swsoyee@gmail.com>
2020-01-31 22:12:50 +0900

    [style] 色変更

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ source(file = "global.R",
        local = TRUE,
        encoding = "UTF-8")
 
-shinyUI(dashboardPage(
+shinyUI(dashboardPage(skin = "purple",
   dashboardHeader(title = lang[[langCode]][1]), # 2019-nCov 日本
   dashboardSidebar(disable = T),
   dashboardBody(fluidPage(fluidRow(
@@ -13,7 +13,6 @@ shinyUI(dashboardPage(
   ),fluidRow(
     box(
       width = 8,
-      status = "primary",
       title = lang[[langCode]][2], # 新型コロナウイルス感染症マップ
       plotOutput("map"),
       footer = paste(lang[[langCode]][5] # ソース：厚生労働省
@@ -21,12 +20,10 @@ shinyUI(dashboardPage(
     ),
     box(
       width = 4,
-      status = 'primary',
       dataTableOutput('totalConfirmedByProvince')
     ),
     box(
       width = 12,
-      status = 'info',
       title = paste0(lang[[langCode]][3], '*'), # 確認累積数
       plotlyOutput("confirmedAccumulation"),
       # 無症状病原体保有者を含む
