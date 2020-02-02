9207c8b3a2c3c595121d70e1919ad494225224b1
swsoyee <swsoyee@gmail.com>
2020-02-02 14:13:48 +0900

    [docs] コード整理

--- global.R
+++ global.R
@@ -8,21 +8,29 @@ library(ggplot2)
 library(shinycssloaders)
 library(shinydashboardPlus)
 
+# ====
+# ファイルのパス設定
+# ====
 DATA_PATH <- 'Data/'
 COMPONENT_PATH <- 'Components/'
 PAGE_PATH <- 'Pages/'
 
+# ====
+# データの読み込み
+# ====
 db <- fread(paste0(DATA_PATH, 'summary.csv'), header = T)
 db[is.na(db)] <- 0
 
 lang <- fread(paste0(DATA_PATH, 'lang.csv'))
-languageSet <- c('ja', 'cn')
 langCode <- 'ja'
-names(languageSet) <- c(lang[[langCode]][25], lang[[langCode]][26])
+# TODO 言語切り替え機能
+# languageSet <- c('ja', 'cn')
+# names(languageSet) <- c(lang[[langCode]][25], lang[[langCode]][26])
 
 news <- fread(paste0(DATA_PATH, 'mhlw_houdou.csv'))
 
-province <- fread(paste0(DATA_PATH, 'provinceCode.csv'), na.strings = NULL)
+province <-
+  fread(paste0(DATA_PATH, 'provinceCode.csv'), na.strings = NULL)
 # Scale設定
 province[, Scale := c(3, rep(1, 46))]
 # 区域名変更
@@ -32,8 +40,13 @@ province[, Prefecture := gsub("東京都", "東京", province$Prefecture)]
 # データ追加
 province[, Data := rowSums(db[, 2:ncol(db)])]
 
+# ====
+# 定数設定
+# ====
 UPDATE_TIME <- '2020-2-1'
 GLOABLE_MAIN_COLOR <- '#605ca8'
-GLOABLE_MAIN_COLOR_RGBVALUE <- paste(as.vector(col2rgb(GLOABLE_MAIN_COLOR)), collapse = ",")
-GLOABLE_MAIN_COLOR_RGBA <- paste0('rgba(', GLOABLE_MAIN_COLOR_RGBVALUE,',0.5)')
-options(spinner.color=GLOABLE_MAIN_COLOR)
+GLOABLE_MAIN_COLOR_RGBVALUE <-
+  paste(as.vector(col2rgb(GLOABLE_MAIN_COLOR)), collapse = ",")
+GLOABLE_MAIN_COLOR_RGBA <-
+  paste0('rgba(', GLOABLE_MAIN_COLOR_RGBVALUE, ',0.5)')
+options(spinner.color = GLOABLE_MAIN_COLOR)
