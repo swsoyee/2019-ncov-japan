0f05548f190972d10abac6dbd2cd20efbc4eef0b
swsoyee <swsoyee@gmail.com>
2020-02-17 23:03:14 +0900

    [chore] 更新日時を自動化に

--- global.R
+++ global.R
@@ -64,8 +64,8 @@ detailSummary <- detail[, .(count = .N), by = .(gender, age)]
 # ====
 # 定数設定
 # ====
-UPDATE_DATE <- '2020-2-17'
-UPDATE_DATETIME <- '2020-2-17 18:30'
+UPDATE_DATETIME <- file.info(paste0(DATA_PATH, 'summary.csv'))$mtime
+UPDATE_DATE <- as.Date(UPDATE_DATETIME)
 GLOABLE_MAIN_COLOR <- '#605ca8'
 GLOABLE_MAIN_COLOR_RGBVALUE <-
   paste(as.vector(col2rgb(GLOABLE_MAIN_COLOR)), collapse = ",")
