2a58c8bc5ec621708d3fd5b52a754eec7a7cda03
swsoyee <swsoyee@gmail.com>
2020-03-08 13:26:52 +0900

    [style] UI調整

--- Pages/Japan.R
+++ Pages/Japan.R
@@ -16,89 +16,103 @@ fluidPage(
   #       ))
   # ),
   fluidRow(
-    widgetUserBox(
-      title = lang[[langCode]][17],
-      # 新型コロナウイルス
-      subtitle = lang[[langCode]][18],
-      # 2019 nCoV
-      width = 12,
-      type = NULL,
-      src = 'ncov.jpeg',
-      color = "purple",
-      collapsible = F,
-      background = T,
-      backgroundUrl = 'ncov_back.jpg',
-      # tags$p(dashboardLabel(status = 'danger',  # APIアクセスできなかった
-      #                       style = 'square', 
-      #                       paste(sep = ' | ', lang[[langCode]][71], # ページ閲覧数
-      #                             statics$result$totals$pageviews$all)
-      #                       ),
-      #        dashboardLabel(status = 'success',
-      #                       style = 'square',
-      #                       paste(sep = ' | ', lang[[langCode]][72], # 閲覧者数
-      #                             statics$result$totals$uniques)
-      #        )
-      #        ),
-      tags$p(tags$img(src = 'https://img.shields.io/badge/dynamic/json?url=https://stg.covid-2019.live/ncov-static/stats.json&label=%E9%96%B2%E8%A6%A7%E6%95%B0&query=$.result.totals.pageviews.all&color=orange&style=flat-square')),
-      # 発熱や上気道症状を引き起こすウイルス...
-      tags$p(lang[[langCode]][19]),
-      tags$a(href = lang[[langCode]][21], # https://www.mhlw.go.jp/stf/...
-             paste0(lang[[langCode]][20], # 出典
-                    '：', lang[[langCode]][22], # コロナウイルスはどのようなウイルスですか？
-                    '（', lang[[langCode]][5], # 厚生労働省
-                    '）、 ')),
-      tags$a(href = lang[[langCode]][59], # https://phil.cdc.gov/Details.aspx?pid=2871
-             # 背景画像
-             lang[[langCode]][58]),
-      tags$br(),
-      actionButton(inputId = 'twitterShare',
-                   label = 'Twiiter',
-                   icon = icon('twitter'),
-                   onclick = sprintf("window.open('%s')", twitterUrl)
+    column(width = 7, style='padding:0px;',
+      widgetUserBox(
+        title = lang[[langCode]][17],
+        # 新型コロナウイルス
+        subtitle = lang[[langCode]][18],
+        # 2019 nCoV
+        width = 12,
+        type = NULL,
+        src = 'ncov.jpeg',
+        color = "purple",
+        collapsible = F,
+        background = T,
+        backgroundUrl = 'ncov_back.jpg',
+        # tags$p(dashboardLabel(status = 'danger',  # APIアクセスできなかった
+        #                       style = 'square', 
+        #                       paste(sep = ' | ', lang[[langCode]][71], # ページ閲覧数
+        #                             statics$result$totals$pageviews$all)
+        #                       ),
+        #        dashboardLabel(status = 'success',
+        #                       style = 'square',
+        #                       paste(sep = ' | ', lang[[langCode]][72], # 閲覧者数
+        #                             statics$result$totals$uniques)
+        #        )
+        #        ),
+        tags$p(tags$img(src = 'https://img.shields.io/badge/dynamic/json?url=https://stg.covid-2019.live/ncov-static/stats.json&label=%E9%96%B2%E8%A6%A7%E6%95%B0&query=$.result.totals.pageviews.all&color=orange&style=flat-square')),
+        # 発熱や上気道症状を引き起こすウイルス...
+        tags$p(lang[[langCode]][19]),
+        footer = tagList(
+          tags$a(href = lang[[langCode]][21], # https://www.mhlw.go.jp/stf/...
+                 icon('link'), 
+                 paste0(lang[[langCode]][22], # コロナウイルスはどのようなウイルスですか？
+                        '（', lang[[langCode]][5], # 厚生労働省
+                        '）、 ')),
+          tags$a(href = lang[[langCode]][59], # https://phil.cdc.gov/Details.aspx?pid=2871
+                 icon('image'), 
+                 # 背景画像
+                 lang[[langCode]][58])
+        )
+      )
+    ),
+    column(
+      width = 5,
+      fluidRow(
+        valueBox(
+          width = 6,
+          value = sum(PCR_WITHIN$final, PCR_FLIGHT$final, PCR_SHIP$final),
+          subtitle = paste0(lang[[langCode]][90], ' (+', sum(PCR_WITHIN$diff, PCR_FLIGHT$diff, PCR_SHIP$diff), ')'),
+          icon = icon('vials'),
+          color = "yellow"
+        ),
+        valueBox(
+          width = 6,
+          value = TOTAL_JAPAN,
+          subtitle = paste0(lang[[langCode]][60], ' (+', TOTAL_JAPAN_DIFF, ')'),
+          icon = icon('procedures'),
+          color = "red"
+        )
       ),
-      actionButton(inputId = 'github',
-                   label = 'Github',
-                   icon = icon('github'),
-                   onclick = sprintf("window.open('%s')", 'https://github.com/swsoyee/2019-ncov-japan')
-                   ),
-      footer_padding = F
-    )
-  ),
-  # バリューボックス
-  fluidRow(
-    valueBox(
-      width = 3,
-      value = sum(PCR_WITHIN$final, PCR_FLIGHT$final, PCR_SHIP$final), # 開発待ち
-      subtitle = paste0(lang[[langCode]][90], ' (+', sum(PCR_WITHIN$diff, PCR_FLIGHT$diff, PCR_SHIP$diff), ')'),
-      icon = icon('vials'),
-      color = "yellow"
-    ),
-    valueBox(
-      width = 3,
-      value = TOTAL_JAPAN,
-      subtitle = paste0(lang[[langCode]][60], ' (+', TOTAL_JAPAN_DIFF, ')'),
-      icon = icon('procedures'),
-      color = "red"
-    ),
-    valueBox(
-      width = 3,
-      value = DISCHARGE_TOTAL, 
-      subtitle = paste0(lang[[langCode]][6],
-                        ' (+', 
-                        SYMPTOM_DISCHARGE_FLIGHT$diff + 
-                          SYMPTOM_DISCHARGE_WITHIN$diff + 
-                          SYMPTOMLESS_DISCHARGE_FLIGHT$diff + 
-                          SYMPTOMLESS_DISCHARGE_WITHIN$diff +
-                          DISCHARGE_SHIP$diff, ')'),
-      icon = icon('thumbs-up'),
-      color = "green"
-    ),
-    valueBox(
-      width = 3,
-      value = DEATH_JAPAN, 
-      subtitle = paste0(lang[[langCode]][7], ' (+', DEATH_JAPAN_DIFF, ')'),
-      icon = icon('bible'),
-      color = "navy"
+      fluidRow(
+        valueBox(
+          width = 6,
+          value = DISCHARGE_TOTAL,
+          subtitle = paste0(lang[[langCode]][6],
+                            ' (+',
+                            SYMPTOM_DISCHARGE_FLIGHT$diff +
+                              SYMPTOM_DISCHARGE_WITHIN$diff +
+                              SYMPTOMLESS_DISCHARGE_FLIGHT$diff +
+                              SYMPTOMLESS_DISCHARGE_WITHIN$diff +
+                              DISCHARGE_SHIP$diff, ')'),
+          icon = icon('user-shield'),
+          color = "green"
+        ),
+        valueBox(
+          width = 6,
+          value = DEATH_JAPAN,
+          subtitle = paste0(lang[[langCode]][7], ' (+', DEATH_JAPAN_DIFF, ')'),
+          icon = icon('bible'),
+          color = "navy"
+        )
+      ),
+      fluidRow(
+        column(width = 12, style='padding:0px;',
+        boxPlus(
+          width = 12,
+          actionButton(inputId = 'twitterShare',
+                       label = 'Twitter',
+                       icon = icon('twitter'),
+                       onclick = sprintf("window.open('%s')", twitterUrl)
+          ),
+          actionButton(inputId = 'github',
+                       label = 'Github',
+                       icon = icon('github'),
+                       onclick = sprintf("window.open('%s')", 'https://github.com/swsoyee/2019-ncov-japan')
+          )
+        )
+        )
+      )
     )
   ),
   fluidRow(
@@ -150,7 +164,7 @@ fluidPage(
             ),
             tabPanel(
               # 退院者推移
-              title = lang[[langCode]][89], icon = icon('thumbs-up'),
+              title = lang[[langCode]][89], icon = icon('user-shield'),
               value = 'discharged',
               fluidRow(
                 column(width = 12,
@@ -164,7 +178,7 @@ fluidPage(
         ),
         column(
           width = 4,
-          tags$b(paste0('感染者なしの地域：', length(regionZero), ' (', round(length(regionZero)/47 * 100, 2), '%)')),
+          tags$b(paste0(lang[[langCode]][97], length(regionZero), ' (', round(length(regionZero)/47 * 100, 2), '%)')),
           uiOutput('saveArea'),
           tags$br(),
           tags$b('感染者'),
