6969be28d7a8af5b6edf9feb96fcb5916658d729
swsoyee <swsoyee@gmail.com>
2020-03-17 16:47:05 +0900

    [update] ツイーターカード仕様変更

--- ui.R
+++ ui.R
@@ -68,7 +68,7 @@ shinyUI(
     dashboardBody(
       tags$head(
         tags$link(rel = "icon", href = "favicon.ico"),
-        tags$meta(name = 'twitter:card', content = 'summary'),
+        tags$meta(name = 'twitter:card', content = 'summary_large_image'),
         tags$meta(property = 'og:url', content = 'https://covid-2019.live/'),
         tags$meta(property = 'og:title', content = '🦠新型コロナウイルス感染速報'),
         tags$meta(property = 'og:description', content = '日本における新型コロナウイルスの最新感染・罹患情報をいち早く速報・まとめるサイトです。'),
