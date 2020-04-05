92061dbc314acd46c878b1e48dad62d8427e26bc
swsoyee <swsoyee@gmail.com>
2020-04-06 00:12:52 +0900

    [feat] アイコン追加

--- Pages/Pref/Hokkaido-Server.R
+++ Pages/Pref/Hokkaido-Server.R
@@ -6,7 +6,7 @@ observeEvent(input$sideBarTab, {
     GLOBAL_VALUE$hokkaidoData$date <- as.Date(paste0(GLOBAL_VALUE$hokkaidoData$年, '/', GLOBAL_VALUE$hokkaidoData$月, '/', GLOBAL_VALUE$hokkaidoData$日))
     # data <- GLOBAL_VALUE$hokkaidoData # TEST
     GLOBAL_VALUE$hokkaidoPatients <- fread(file = paste0(DATA_PATH, 'Pref/Hokkaido/patients.csv'))
-    data <- GLOBAL_VALUE$hokkaidoPatients # TEST
+    # data <- GLOBAL_VALUE$hokkaidoPatients # TEST
   }
 })
 
@@ -228,7 +228,8 @@ output$hokkaidoProfile <- renderUI({
       width = 12, 
       closable = F,
       boxProfile(
-        title = paste0('北海道', profile$No),
+        title = paste0('北海道', profile$No), 
+        src = ifelse(profile$性別.x == '男性', 'Icon/male.png', 'Icon/female.png'),
         subtitle = tagList(profile$性別.x),
         boxProfileItemList(
           bordered = TRUE,
@@ -272,15 +273,19 @@ output$hokkaidoProfile <- renderUI({
 
 output$hokkaidoPatientTable <- renderDataTable({
   data <- hokkaidoData()$patient
-  testData <- data
+  # testData <- data
   
   showCols <- c('No', 'リリース日', '居住地.x', '年代.x', '性別.x')
-  dataForShow <- testData[, showCols, with = F]
+  dataForShow <- data[, showCols, with = F]
   colnames(dataForShow) <- c('自治体番号', '公表日', '居住地', '年代', '性別')
   dataForShow$公表日 <- as.Date(dataForShow$公表日)
+  dataForShow$居住地 <- as.factor(dataForShow$居住地)
+  dataForShow$年代 <- as.factor(dataForShow$年代)
+  dataForShow$性別 <- as.factor(dataForShow$性別)
   DT::datatable(
     dataForShow,
     rownames = F,
+    filter = 'top',
     extensions = c('Responsive'),
     options = list(
       dom = 't',
@@ -293,4 +298,58 @@ output$hokkaidoPatientTable <- renderDataTable({
   )
 })
 
-
+# ====作成中====
+# output$hokkaidoGenderBar <- renderEcharts4r({
+#   data <- hokkaidoData()$patient
+#   
+#   dt <- data[, .(count = .N), by = .(年代.x, 性別.x)]
+#   
+#   maleCount <- sum(dt$count.男性)
+#   femaleCount <- sum(dt$count.女性)
+#   totalCount <- maleCount + femaleCount
+#   dt %>%
+#     e_chart(年齢) %>%
+#     e_bar(count.男, stack = '1', name = '男性', itemStyle = list(color = darkNavy)) %>%
+#     e_bar(count.女性, stack = '1', name = '女性', itemStyle = list(color = middleRed)) %>%
+#     e_x_axis(axisTick = list(show = F), offset = - 20) %>%
+#     e_labels(position = 'inside', formatter = htmlwidgets::JS('
+#       function(params) {
+#         let count = params.value[0]
+#         if(count < 0) {
+#           count = -count
+#         }
+#         return(count)
+#       }
+#     ')) %>%
+#     e_y_axis(show = F) %>%
+#     e_flip_coords() %>%
+#     e_tooltip(formatter = htmlwidgets::JS(paste0('
+#       function(params) {
+#         let count = params[0].value[0]
+#         if(count < 0) {
+#           count = -count
+#         }
+#         const total = Number(count) + Number(params[1].value[0])
+#         return(`${params[0].value[1]}合計：${total}人 (総計の${Math.round(total/', totalCount, '*100, 4)}%)
+#           <hr>男性：${count}人 (${params[0].value[1]}の${Math.round(count/total*100, 4)}%)
+#           <br>女性：${params[1].value[0]}人 (${params[0].value[1]}の${Math.round(params[1].value[0]/total*100, 4)}%)
+#         `)
+#       }
+#                                           ')), 
+#               trigger = 'axis',
+#               axisPointer = list(type = 'shadow')
+#     ) %>%
+#     e_title(
+#       text = paste0(
+#         '男性：', maleCount, '人 (', round(maleCount / totalCount * 100, 2), 
+#         '%), 女性：', femaleCount, '人 (', round(femaleCount / totalCount * 100, 2), 
+#         '%), 計：', totalCount, '人'),
+#       textStyle = list(fontSize = 11),
+#       subtext = '性別、年齢不明および発表なしの感染者が含まれていませんのでご注意ください。',
+#       subtextStyle = list(fontSize = 9)
+#     ) %>%
+#     e_legend(top = '15%', right = '0%', selectedMode = F, orient = 'vertical') %>%
+#     e_grid(bottom = '0%', right = '5%', left = '5%')
+# })
+# 
+# 
