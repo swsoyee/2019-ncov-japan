df7f92b3a4a67d6e7fca0ca00b217c08abcb95c1
swsoyee <swsoyee@gmail.com>
2020-04-16 22:19:38 +0900

    [feat] 検査人数に移動平均ラインを追加

--- Components/Main/Tendency.Test.server.R
+++ Components/Main/Tendency.Test.server.R
@@ -1,25 +1,27 @@
 # ====PCR検査数の推移図データセット====
 pcrData <- reactive({
   dt <-
-    merge(x = domesticDailyReport,
-          y = flightDailyReport,
-          by = 'date',
-          all.x = T)
+    merge(
+      x = domesticDailyReport,
+      y = flightDailyReport,
+      by = "date",
+      all.x = T
+    )
   dt <-
     merge(
       x = dt,
       y = airportDailyReport,
-      by = 'date',
-      suffixes = '.y',
+      by = "date",
+      suffixes = ".y",
       all.x = T
     )
   dt <-
     merge(
       x = dt,
       y = shipDailyReport,
-      by = 'date',
+      by = "date",
       all.x = T,
-      suffixes = '.z'
+      suffixes = ".z"
     )
   dataset <- domesticDailyReport
   if (input$showFlightInPCR) {
@@ -40,46 +42,51 @@ pcrData <- reactive({
 # ====PCR検査数====
 output$pcrLine <- renderEcharts4r({
   dt <- pcrData()
-  
+  dt$ma <- round(frollmean(dt$diff, n = input$testDaySpan, fill = 0), 2)
   dt %>%
     e_chart(date) %>%
-    e_line(
+    e_bar(
       pcr,
-      name = '累計',
-      itemStyle = list(color = lightYellow),
-      areaStyle = list(opacity = 0.5)
+      name = "累積",
+      itemStyle = list(color = lightYellow)
     ) %>%
     e_bar(
       diff,
-      name = '新規（日次）',
-      y_index = 1,
-      itemStyle = list(color = middleYellow)
+      name = "新規",
+      itemStyle = list(color = darkYellow),
+      z = 2, barGap = "-100%"
     ) %>%
+    e_line(ma, name = paste0(input$testDaySpan, "日移動平均（新規）"), y_index = 1, symbol = "none", smooth = T, itemStyle = list(color = darkRed)) %>%
     e_x_axis(splitLine = list(lineStyle = list(opacity = 0.2))) %>%
     e_y_axis(
-      name = '累積検査人数',
+      name = "検査人数",
       nameGap = 10,
-      nameTextStyle = list(padding = c(0, 0, 0, 80)),
+      nameTextStyle = list(padding = c(0, 0, 0, 50)),
       splitLine = list(lineStyle = list(opacity = 0.2)),
       axisLabel = list(inside = T),
+      min = -5000,
       axisTick = list(show = F)
     ) %>%
     e_y_axis(
-      name = '日次新規検査人数',
+      name = "移動平均新規数",
       nameGap = 10,
       splitLine = list(show = F),
       index = 1,
+      min = -250,
       axisTick = list(show = F)
     ) %>%
-    e_grid(left = '3%',
-           right = '15%',
-           bottom = '10%') %>%
-    e_legend(
-      type = 'scroll',
-      orient = 'vertical',
-      left = '10%',
-      top = '15%'
+    e_grid(
+      left = "3%",
+      right = "15%",
+      bottom = "10%"
     ) %>%
-    e_tooltip(trigger = 'axis') %>%
-    e_title(text = '日次新規・累積検査人の推移')
+    e_legend(
+      type = "scroll",
+      orient = "vertical",
+      left = "18%",
+      top = "15%",
+      right = "15%"
+    ) %>%
+    e_tooltip(trigger = "axis") %>%
+    e_title(text = "日次新規・累積検査人数の推移")
 })
--- Components/Main/Tendency.Test.ui.R
+++ Components/Main/Tendency.Test.ui.R
@@ -1,53 +1,66 @@
 tabPanel(
   # PCR検査数推移
-  title = 'PCR検査数の推移',
-  icon = icon('vials'),
-  value = 'pcr',
+  title = "PCR検査数の推移",
+  icon = icon("vials"),
+  value = "pcr",
   fluidRow(
     column(
       width = 8,
       tags$br(),
       fluidRow(
         column(
-          width = 6,
-          tags$p(tags$b('クルーズ・チャーター便事例の表示')),
+          width = 4,
+          tags$p(tags$b("クルーズ・チャーター便事例の表示")),
           switchInput(
             inputId = "showShipInPCR",
-            label = icon('ship'), 
-            offLabel = icon('eye-slash'), 
-            onLabel = icon('eye'),
+            label = icon("ship"),
+            offLabel = icon("eye-slash"),
+            onLabel = icon("eye"),
             value = F,
             inline = T
           ),
           switchInput(
             inputId = "showFlightInPCR",
-            label = icon('plane'),
-            offLabel = icon('eye-slash'), 
-            onLabel = icon('eye'),
+            label = icon("plane"),
+            offLabel = icon("eye-slash"),
+            onLabel = icon("eye"),
             value = T,
             inline = T
           )
+        ),
+        column(
+          width = 4,
+          sliderInput(
+            inputId = "testDaySpan",
+            label = "移動平均時間間隔",
+            min = 1,
+            max = 10,
+            value = 4,
+            ticks = F,
+            step = 1,
+            post = "日"
+          )
         )
       ),
-      echarts4rOutput('pcrLine') %>% withSpinner()
+      echarts4rOutput("pcrLine") %>% withSpinner()
     ),
     column(
       width = 4,
       tagList(
         tags$br(),
-        tags$b('注意点'),
+        tags$b("注意点"),
         tags$li(lang[[langCode]][98]), # 「令和２年３月４日版」以後は、陽性となった者の~
         tags$li(lang[[langCode]][99]), # これまで延べ人数で公表しましたクルーズ船のＰＣＲ~
         tags$br(),
         tags$a(
-          href = 'https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000121431_00086.html',
-          icon('link'),
-          '報道発表一覧（新型コロナウイルス）'
+          href = "https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000121431_00086.html",
+          icon("link"),
+          "報道発表一覧（新型コロナウイルス）"
         ),
         tags$hr(),
-        tags$b('PCR検査数（日次）')
+        tags$b("PCR検査数（日次）")
       ),
-      echarts4rOutput('pcrCalendar', height = '130px') %>% withSpinner()
+      echarts4rOutput("pcrCalendar", height = "130px") %>% withSpinner()
     )
   )
 )
