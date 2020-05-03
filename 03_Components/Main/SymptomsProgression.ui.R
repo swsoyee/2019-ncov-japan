e8223a60f9c59b4033ae70a1fd3192ba7dccceac
swsoyee <swsoyee@gmail.com>
2020-05-03 14:20:38 +0900

    [add] Sankey図の説明文言に翻訳追加

--- 03_Components/Main/SymptomsProgression.ui.R
+++ 03_Components/Main/SymptomsProgression.ui.R
@@ -11,28 +11,12 @@ Component.SymptomsProgression <- function() {
       min = domesticDailyReport$date[1],
       max = domesticDailyReport$date[nrow(domesticDailyReport)],
       value = domesticDailyReport$date[nrow(domesticDailyReport)],
-      language = "ja"
+      language = i18n$translation_language
     ),
-    tags$small(
-      paste0(
-        "3月28日以後、厚労省集計方法が変更あり、",
-        "無症状患者の内訳は公表しないため、",
-        "無症状患者がどのぐらい入院しているかの情報がなくなりました。",
-        "こちらではグラフ作成するため、無症状者は一律入院必要ないという仮説を設定した上で、グラフを作りました。",
-        "よって、該当グラフはあくまで参考です。"
-      )
-    ),
+    tags$small(i18n$t("3月28日以後、厚労省集計方法が変更あり、無症状患者の内訳は公表しないため、無症状患者がどのぐらい入院しているかの情報がなくなりました。こちらではグラフ作成するため、無症状者は一律入院必要ないという仮説を設定した上で、グラフを作りました。よって、該当グラフはあくまで参考です。")),
     tags$br(),
-    tags$small(
-      paste0(
-        "4月22日以後、厚労省集計方法が変更あり、",
-        "退院者、死亡者数に突合作業中の数値を含めているが、",
-        "分母（PCR陽性者）と一致しないという説明があったため",
-        "こちらではグラフ作成するため、今まで通り確定した人数のみでグラフを作りました。",
-        "よって、該当グラフはあくまで参考です。"
-      )
-    ),
+    tags$small(i18n$t("4月22日以後、厚労省集計方法が変更あり、退院者、死亡者数に突合作業中の数値が含まれ、分母（PCR陽性者）と一致しないという説明がありました。こちらではグラフの作成するため、今まで通り確定した人数のみでグラフを作成します。よって、該当グラフはあくまで参考です。")),
     echarts4rOutput("processSankey") %>% withSpinner(),
-    footer = tags$small("※開発バージョンです。")
+    footer = tags$small(i18n$t("※開発バージョンです。"))
   )
 }
