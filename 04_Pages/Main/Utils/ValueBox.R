a14287cf29a8106f802325a00382a97b23a349ea
swsoyee <swsoyee@gmail.com>
2020-05-03 14:47:32 +0900

    [add] 文言追加

--- 04_Pages/Main/Utils/ValueBox.R
+++ 04_Pages/Main/Utils/ValueBox.R
@@ -21,7 +21,7 @@ Component.MainValueBox <-
           diffNumber,
           getChangeIconWrapper(diffNumber),
           text,
-          style =  "float:right;"
+          style = "float:right;"
         )
       ),
       icon = icon(icon),
@@ -38,19 +38,17 @@ Component.MainValueBox.Info <-
            text,
            icon,
            color) {
-    
+
     valueBox(
       width = 6,
       value = tagList(
         mainValue,
-        tags$small(paste0("| ", mainValueSub), 
-                   tags$span(id = "discharged_info", icon("info-circle")), 
-                   bsTooltip(id = "discharged_info", 
-                             title = paste0("確定済みの退院者数は", mainValueSub, "名、他にも", 
-                             (mainValue - mainValueSub), 
-                             "名の退院者は個々の陽性者との突合作業中。従って、入退院等の状況の合計とPCR検査陽性者数は一致しない。",
-                             "<br><br>※令和２年４月２２日から厚労省公開している退院者数の基準が変わりました。"), 
-                             placement = "top"),
+        tags$small(paste0("| ", mainValueSub),
+                   tags$span(id = "discharged_info", icon("info-circle")),
+                   bsTooltip(id = "discharged_info",
+                             title = sprintf(i18n$t("確定済みの退院者数は%s名、他にも%s名の退院者は個々の陽性者との突合作業中。従って、入退院等の状況の合計とPCR検査陽性者数は一致しない。<br><br>※令和２年４月２２日から厚労省公開している退院者数の基準が変わりました。"),
+                             mainValueSub, (mainValue - mainValueSub)),
+                             placement = "right"),
                    style = "color:white;font-size:16px;margin-top:10px;margin-right:10px;opacity:0.8"
         )
       ),
@@ -61,7 +59,7 @@ Component.MainValueBox.Info <-
           diffNumber,
           getChangeIconWrapper(diffNumber),
           text,
-          style =  "float:right;"
+          style = "float:right;"
         )
       ),
       icon = icon(icon),
