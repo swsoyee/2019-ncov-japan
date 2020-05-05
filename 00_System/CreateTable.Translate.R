e44b3ab44af4c9717d3eff3da32d5a53d116ebd5
swsoyee <swsoyee@gmail.com>
2020-05-05 18:19:18 +0900

    [add] テーブル文言、次いでに北海道−１

--- 00_System/CreateTable.Translate.R
+++ 00_System/CreateTable.Translate.R
@@ -16,6 +16,10 @@ prefVector <- c("北海道", "青森", "岩手", "宮城", "秋田", "山形", "
                 "奈良", "和歌山", "鳥取", "島根", "岡山", "広島", "山口",
                 "徳島", "香川", "愛媛", "高知", "福岡", "佐賀", "長崎",
                 "熊本", "大分", "宮崎", "鹿児島", "沖縄", "クルーズ船", "検疫職員", "チャーター便", "伊客船")
+tooltip <- list(
+  "cn" = list("新規" = "新增", "累計" = "累计", "名" = "人", "情報待ち陽性者" = "等待消息的阳性患者", "入院者" = "住院", "退院者" = "出院", "退院" = "出院", "死亡者" = "死亡"),
+  "en" = list("新規" = "New ", "累計" = "Total ", "名" = "", "情報待ち陽性者" = "Positive (more information needed) ", "入院者" = "Hospitalized ", "退院者" = "Discharged ", "退院" = "Discharged ", "死亡者" = "Death ")
+)
 
 for (language in c("en", "cn")) {
   dt <- copy(data)
@@ -24,6 +28,11 @@ for (language in c("en", "cn")) {
   }
   for (i in prefVector) {
     dt$region <- gsub(i, eval(as.name(language))[which(i == ja)], dt$region)
+  }
+  for (i in seq(tooltip[[language]])) {
+    dt$diff <- gsub(names(tooltip[[language]][i]), tooltip[[language]][i], dt$diff)
+    dt$dischargeDiff <- gsub(names(tooltip[[language]][i]), tooltip[[language]][i], dt$dischargeDiff)
+    dt$detailBullet <- gsub(names(tooltip[[language]][i]), tooltip[[language]][i], dt$detailBullet)
   }
   fwrite(x = dt, file = paste0(DATA_PATH, paste0("Generated/resultSummaryTable.", language, ".csv")), sep = "@", quote = F)
 }
