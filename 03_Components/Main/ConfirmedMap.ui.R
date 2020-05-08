1a648bc87f03105b5c3e6a460b2f2aa75776c4a1
swsoyee <swsoyee@gmail.com>
2020-05-08 22:31:06 +0900

    [style] change patients at present  bar and other ui

--- 03_Components/Main/ConfirmedMap.ui.R
+++ 03_Components/Main/ConfirmedMap.ui.R
@@ -12,7 +12,7 @@ tabPanel(
             width = 6,
             switchInput(
               inputId = "switchMapVersion",
-              value = T, 
+              value = T,
               onLabel = i18n$t("シンプル"),
               onStatus = "danger",
               offStatus = "danger",
@@ -28,48 +28,48 @@ tabPanel(
           column(
             width = 6,
             tags$span(
-            dropdownButton(
-              tags$h4(i18n$t("表示設定")),
-              materialSwitch(
-                inputId = "showPopupOnMap",
-                label = i18n$t("日次増加数のポップアップ"),
+              dropdownButton(
+                tags$h4(i18n$t("表示設定")),
+                materialSwitch(
+                  inputId = "showPopupOnMap",
+                  label = i18n$t("日次増加数のポップアップ"),
+                  status = "danger",
+                  value = T
+                ),
+                materialSwitch(
+                  inputId = "replyMapLoop",
+                  label = i18n$t("ループ再生"),
+                  status = "danger",
+                  value = T
+                ),
+                dateRangeInput(
+                  inputId = "mapDateRange",
+                  label = i18n$t("表示日付"),
+                  start = byDate$date[nrow(byDate) - 15],
+                  end = byDate$date[nrow(byDate)],
+                  min = byDate$date[1],
+                  max = byDate$date[nrow(byDate)],
+                  separator = " ~ ",
+                  language = "ja"
+                ),
+                sliderInput(
+                  inputId = "mapFrameSpeed",
+                  label = i18n$t("再生速度（秒/日）"),
+                  min = 0.5,
+                  max = 3,
+                  step = 0.1,
+                  value = 0.8
+                ),
+                circle = F,
+                inline = T,
                 status = "danger",
-                value = T
+                icon = icon("gear"),
+                size = "sm",
+                width = "300px",
+                tooltip = tooltipOptions(title = i18n$t("表示設定"), placement = "top")
               ),
-              materialSwitch(
-                inputId = "replyMapLoop",
-                label = i18n$t("ループ再生"),
-                status = "danger",
-                value = T
-              ),
-              dateRangeInput(
-                inputId = "mapDateRange",
-                label = i18n$t("表示日付"),
-                start = byDate$date[nrow(byDate) - 15],
-                end = byDate$date[nrow(byDate)],
-                min = byDate$date[1],
-                max = byDate$date[nrow(byDate)],
-                separator = " ~ ",
-                language = "ja"
-              ),
-              sliderInput(
-                inputId = "mapFrameSpeed",
-                label = i18n$t("再生速度（秒/日）"),
-                min = 0.5,
-                max = 3,
-                step = 0.1,
-                value = 0.8
-              ),
-              circle = F,
-              inline = T,
-              status = "danger",
-              icon = icon("gear"),
-              size = "sm",
-              width = "300px",
-              tooltip = tooltipOptions(title = i18n$t("表示設定"), placement = "top")
-            ),
-            style = "float:right;"
-          )
+              style = "float:right;"
+            )
           ),
         ),
         style = "margin-top:10px;"
@@ -77,16 +77,23 @@ tabPanel(
       uiOutput("comfirmedMapWrapper") %>% withSpinner(proxy.height = "500px"),
       # TODO もし全部の都道府県に感染者報告がある場合、こちらのバーを再検討する
       progressBar(
-        id = "hasConfirmedRegionBar",
-        value = 47 - length(regionZero),
-        total = 47,
+        id = "activePatients",
+        value = TOTAL_JAPAN - DEATH_JAPAN - 40 -
+          (tail(confirmingData$domesticDischarged, n = 1) +
+            (DISCHARGE_TOTAL - DISCHARGE_WITHIN$final)),
+        total = TOTAL_JAPAN - DEATH_JAPAN - 40,
         title = tagList(
-          icon("exclamation-triangle"),
-          i18n$t("感染者報告あり")
+          icon("procedures"),
+          i18n$t("現在患者数")
         ),
         striped = T,
         status = "danger",
         display_pct = T
+      ),
+      bsTooltip(
+        id = "activePatients",
+        placement = "top",
+        title = i18n$t("分母には死亡者、チャーター便で帰国したクルーズ船の乗客40名が含まれていません。")
       ),
       tagList(icon("shield-alt"), tags$b(i18n$t("感染者報告なし"))),
       uiOutput("saveArea"),
@@ -96,10 +103,10 @@ tabPanel(
       boxPad(
         fluidRow(
           column(
-            width = 8,
+            width = 9,
             radioGroupButtons(
               inputId = "switchTableVersion",
-              label = NULL, 
+              label = NULL,
               justified = T,
               choiceNames = c(
                 paste(icon("procedures"), i18n$t("感染")),
@@ -111,7 +118,7 @@ tabPanel(
             )
           ),
           column(
-            width = 4,
+            width = 3,
             tags$span(
               awesomeCheckbox(
                 inputId = "tableShowSetting",
