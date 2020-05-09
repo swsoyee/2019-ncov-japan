695568f4f156e6a620559d38f42a7edf11e5ad3c
swsoyee <swsoyee@gmail.com>
2020-05-09 11:06:47 +0900

    [add] countup effect

--- global.R
+++ global.R
@@ -14,6 +14,7 @@ library(echarts4r)
 library(sparkline)
 library(shinyBS)
 library(shiny.i18n)
+library(countup)
 
 source(file = "01_Settings/Path.R", local = T, encoding = "UTF-8")
 source(file = "02_Utils/Functions.R", local = T, encoding = "UTF-8")
