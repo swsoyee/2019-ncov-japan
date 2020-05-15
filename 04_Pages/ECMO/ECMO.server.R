8adc808f06284996e037b7933f034ed5f75c723f
swsoyee <swsoyee@gmail.com>
2020-05-16 00:55:02 +0900

    [feat] ecmo実績グラフ追加

--- 04_Pages/ECMO/ECMO.server.R
+++ 04_Pages/ECMO/ECMO.server.R
@@ -16,7 +16,7 @@ observeEvent(input$sideBarTab, {
       "四国" = c(1, startIndex + 36:39),
       "九州・沖縄" = c(1, startIndex + 40:47)
     )
-    
+
     for (index in seq(length(groupList))) {
       local({
         i <- index
@@ -28,12 +28,14 @@ observeEvent(input$sideBarTab, {
             e_line(value, symbol = "circle", symbolSize = 1) %>%
             e_title(text = sprintf("%s", names(groupList[i]))) %>%
             e_tooltip(trigger = "axis") %>%
-            e_y_axis(max = 100, 
-                     axisTick = list(show = F), 
-                     axisLabel = list(inside = T), 
-                     name = "実施件数",
-                     nameTextStyle = list(padding = c(0, 0, 0, 45)),
-                     splitLine = list(lineStyle = list(opacity = 0.2))) %>%
+            e_y_axis(
+              max = 100,
+              axisTick = list(show = F),
+              axisLabel = list(inside = T),
+              name = "実施件数",
+              nameTextStyle = list(padding = c(0, 0, 0, 45)),
+              splitLine = list(lineStyle = list(opacity = 0.2))
+            ) %>%
             e_legend(
               type = "scroll",
               orient = "vertical",
@@ -43,8 +45,8 @@ observeEvent(input$sideBarTab, {
             ) %>%
             e_grid(right = "2%", left = "2%") %>%
             e_group("artificialRespirators")
-          
-          if(index == max(length(groupList))) {
+
+          if (index == max(length(groupList))) {
             plot %>% e_connect_group("artificialRespirators")
           } else {
             plot
@@ -58,12 +60,14 @@ observeEvent(input$sideBarTab, {
             e_line(value, symbol = "circle", symbolSize = 1) %>%
             e_title(text = sprintf("%s", names(groupList[i]))) %>%
             e_tooltip(trigger = "axis") %>%
-            e_y_axis(max = 20, 
-                     axisTick = list(show = F), 
-                     axisLabel = list(inside = T), 
-                     name = "実施件数",
-                     nameTextStyle = list(padding = c(0, 0, 0, 45)),
-                     splitLine = list(lineStyle = list(opacity = 0.2))) %>%
+            e_y_axis(
+              max = 20,
+              axisTick = list(show = F),
+              axisLabel = list(inside = T),
+              name = "実施件数",
+              nameTextStyle = list(padding = c(0, 0, 0, 45)),
+              splitLine = list(lineStyle = list(opacity = 0.2))
+            ) %>%
             e_legend(
               type = "scroll",
               orient = "vertical",
@@ -73,8 +77,8 @@ observeEvent(input$sideBarTab, {
             ) %>%
             e_grid(right = "2%", left = "2%") %>%
             e_group("ecmoUsing")
-          
-          if(index == max(length(groupList))) {
+
+          if (index == max(length(groupList))) {
             plot %>% e_connect_group("ecmoUsing")
           } else {
             plot
@@ -90,23 +94,24 @@ observeEvent(input$sideBarTab, {
 output$artificialRespirators <- renderUI({
   style <- "padding-left:3px; padding-right:3px; padding-top:1px; padding-bottom:1px"
   tagList(
-      fluidRow(
-        column(3, 
-               tags$br(),
-               tags$b(icon("exclamation-circle"), i18n$t("注意事項")),
-               tags$p("これらのグラフはCRISISに申告された人工呼吸が必要な重症患者さんの推移を地方別、都道府県別に示すものです。同じデータに対して、日本COVID-19対策ECMOnetと若干異なる可視化方法で表現しています。"),
-               blockQuote(tags$small("現在精度を上げるべく努力しております。全体の流れは把握していると自負しておりますが、かならずしも正確な数が示されている訳ではないことをご理解ください。COVID-19では長期の人工呼吸となる患者さんが多い傾向があります。このあたりも今後このコラムでお示ししていけるように計画しております。ーー2020/5/1記載"))
-               ),
-        column(3, echarts4rOutput("artificialRespirators_1"), style = style),
-        column(3, echarts4rOutput("artificialRespirators_2"), style = style),
-        column(3, echarts4rOutput("artificialRespirators_3"), style = "padding-left:3px; padding-top:1px; padding-bottom:1px")
+    fluidRow(
+      column(
+        3,
+        tags$br(),
+        tags$b(icon("exclamation-circle"), i18n$t("注意事項")),
+        tags$p("これらのグラフはCRISISに申告された人工呼吸が必要な重症患者さんの推移を地方別、都道府県別に示すものです。同じデータに対して、日本COVID-19対策ECMOnetと若干異なる可視化方法で表現しています。"),
+        blockQuote(tags$small("現在精度を上げるべく努力しております。全体の流れは把握していると自負しておりますが、かならずしも正確な数が示されている訳ではないことをご理解ください。COVID-19では長期の人工呼吸となる患者さんが多い傾向があります。このあたりも今後このコラムでお示ししていけるように計画しております。ーー2020/5/1記載"))
       ),
-      fluidRow(
-        column(3, echarts4rOutput("artificialRespirators_4"), style = "padding-right:3px; padding-top:1px; padding-bottom:1px"),
-        column(3, echarts4rOutput("artificialRespirators_5"), style = style),
-        column(3, echarts4rOutput("artificialRespirators_6"), style = style),
-        column(3, echarts4rOutput("artificialRespirators_7"), style = "padding-left:3px; padding-top:1px; padding-bottom:1px")
-      )
+      column(3, echarts4rOutput("artificialRespirators_1"), style = style),
+      column(3, echarts4rOutput("artificialRespirators_2"), style = style),
+      column(3, echarts4rOutput("artificialRespirators_3"), style = "padding-left:3px; padding-top:1px; padding-bottom:1px")
+    ),
+    fluidRow(
+      column(3, echarts4rOutput("artificialRespirators_4"), style = "padding-right:3px; padding-top:1px; padding-bottom:1px"),
+      column(3, echarts4rOutput("artificialRespirators_5"), style = style),
+      column(3, echarts4rOutput("artificialRespirators_6"), style = style),
+      column(3, echarts4rOutput("artificialRespirators_7"), style = "padding-left:3px; padding-top:1px; padding-bottom:1px")
+    )
   )
 })
 
@@ -114,10 +119,11 @@ output$ecmoUsing <- renderUI({
   style <- "padding-left:3px; padding-right:3px; padding-top:1px; padding-bottom:1px"
   tagList(
     fluidRow(
-      column(3, 
-             tags$br(),
-             tags$b(icon("exclamation-circle"), i18n$t("注意事項")),
-             blockQuote(tags$small("ECMO装着の方の多くは人工呼吸器も装着しておられるので上記と一部重複のカウントとなります。CRISISに申告のあったECMOの数の推移です。その日のECMOの稼働数とお考えください。CRISISに加入されずECMOを行っている施設も数施設ございます。ーー2020/5/1記載"))
+      column(
+        3,
+        tags$br(),
+        tags$b(icon("exclamation-circle"), i18n$t("注意事項")),
+        blockQuote(tags$small("ECMO装着の方の多くは人工呼吸器も装着しておられるので上記と一部重複のカウントとなります。CRISISに申告のあったECMOの数の推移です。その日のECMOの稼働数とお考えください。CRISISに加入されずECMOを行っている施設も数施設ございます。ーー2020/5/1記載"))
       ),
       column(3, echarts4rOutput("ecmoUsing_1"), style = style),
       column(3, echarts4rOutput("ecmoUsing_2"), style = style),
@@ -130,4 +136,49 @@ output$ecmoUsing <- renderUI({
       column(3, echarts4rOutput("ecmoUsing_7"), style = "padding-left:3px; padding-top:1px; padding-bottom:1px")
     )
   )
+})
+
+output$ecmo <- renderEcharts4r({
+  today <- tail(GLOBAL_VALUE$ECMO$ecmo, n = 1)
+  todaySum <- sum(today[, 2:4])
+  GLOBAL_VALUE$ECMO$ecmo %>%
+    e_chart(日付) %>%
+    e_area(死亡,
+      stack = 1, itemStyle = list(color = darkNavy),
+      symbol = "circle", symbolSize = 1, smooth = T
+    ) %>%
+    e_area(実施中,
+      stack = 1, itemStyle = list(color = darkRed),
+      symbol = "circle", symbolSize = 1, smooth = T
+    ) %>%
+    e_area(離脱,
+      stack = 1, itemStyle = list(color = middleGreen),
+      symbol = "circle", symbolSize = 1, smooth = T
+    ) %>%
+    e_y_axis(
+      axisTick = list(show = F),
+      axisLabel = list(inside = T),
+      splitLine = list(lineStyle = list(opacity = 0.2))
+    ) %>%
+    e_legend(
+      type = "scroll",
+      orient = "vertical",
+      left = "18%",
+      top = "15%",
+      right = "15%"
+    ) %>%
+    e_grid(
+      left = "5%",
+      right = "5%"
+    ) %>%
+    e_tooltip(trigger = "axis") %>%
+    e_title(
+      text = sprintf("%s現在", today$日付),
+      subtext = sprintf(
+        "ECMO離脱 %s 例（%s%%）、ECMO実施中 %s 例（%s%%）、死亡 %s 例（%s%%）",
+        today$離脱, round(today$離脱 / todaySum * 100, 2),
+        today$実施中, round(today$実施中 / todaySum * 100, 2),
+        today$死亡, round(today$死亡 / todaySum * 100, 2)
+      )
+    )
 })
--- 04_Pages/ECMO/ECMO.ui.R
+++ 04_Pages/ECMO/ECMO.ui.R
@@ -1,17 +1,35 @@
 fluidRow(
   boxPlus(
-    width = 12, closable = F, collapsible = T, collapsed = F, enable_label = T, label_text = "表示",
+    width = 12, closable = F, collapsible = T, collapsed = F, 
+    enable_label = T, label_text = tagList("表示", icon('hand-point-right')),
     title = tagList(icon("first-aid"), "COVID-19重症者における人工呼吸器装着数の推移"),
     uiOutput("artificialRespirators") %>% withSpinner()
   ),
   boxPlus(
-    width = 12, closable = F, collapsible = T, collapsed = T, enable_label = T, label_text = "表示",
+    width = 12, closable = F, collapsible = T, collapsed = T, 
+    enable_label = T, label_text = tagList("表示", icon('hand-point-right')),
     title = tagList(icon("heartbeat"), "COVID-19重症者におけるECMO装着数の推移"),
     uiOutput("ecmoUsing") %>% withSpinner()
   ),
   boxPlus(
-    width = 12, closable = F, collapsible = T, collapsed = T, enable_label = T, label_text = "表示",
-    title = tagList(icon("file-medical"), "国内のCOVID-19に対するECMO治療の成績累計")#,
-    # uiOutput("artificialRespirators") %>% withSpinner()
+    width = 12, closable = F, collapsible = T, collapsed = T, 
+    enable_label = T, label_text = tagList("表示", icon('hand-point-right')),
+    title = tagList(icon("file-medical"), "国内のCOVID-19に対するECMO治療の成績累計"),
+    fluidRow(
+      column(
+        width = 4,
+        tags$br(),
+        tags$b(icon("exclamation-circle"), i18n$t("注意事項")),
+        blockQuote(
+          tags$small(
+            "この図はCRISISに申告のあった症例と、それ以外に我々のネットワークで集めたECMO症例の推移をあわらしたものです。あとから判明した症例も多くありますので、過去にさかのぼって日々数が変異しております。したがって上の図の数とここに表す数にも若干の齟齬が生じますのでご了承ください。人工呼吸が必要な患者さんのほぼ5人に1人がECMOも必要と判断されます。ECMOからの生還例ではおおよそ10日間から2週間のECMO装着が必要となります。ーー2020/5/1記載"
+          )
+        )
+        ),
+      column(
+        width = 8,
+        echarts4rOutput("ecmo") %>% withSpinner()
+      )
+    )
   )
 )
