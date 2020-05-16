adeea5e8929be6d0d9c026acf9433b719d6f687a
swsoyee <swsoyee@gmail.com>
2020-05-16 21:49:11 +0900

    [update] 文言追加

--- 03_Components/Main/ConfirmedCityTreemap.server.R
+++ 03_Components/Main/ConfirmedCityTreemap.server.R
@@ -10,8 +10,8 @@ createConfirmedCityTreemap <- function(dataset) {
       left = "1%", right = "1%", bottom = "10%"
     ) %>%
     e_tooltip() %>%
-    e_title(text = paste0("集計時間：", value), 
-            subtext = "データソース：@kenmo_economics　※陽性者数は居住地が判明した方のみを集計しているので、県の総計と一致しません。",
+    e_title(text = i18n$t("5月9までの市区町村の感染者数"), 
+            subtext = i18n$t("データソース：@kenmo_economics\n※陽性者数は居住地が判明した方のみを集計しているので、県の総計と一致しません。\n※5月9日以後のデータは収集されないため、グラフの更新も中止となります。"),
             sublink = "https://twitter.com/kenmo_economics")
 }
 
