a1fecce8ebe4508398ee3b57f323cdfd633c9100
swsoyee <swsoyee@gmail.com>
2020-06-02 23:36:12 +0900

    [feat] date selector in world map

--- 04_Pages/World/World.ui.R
+++ 04_Pages/World/World.ui.R
@@ -3,6 +3,7 @@ fluidPage(
     width = 8, 
     closable = F,
     title = tagList(icon("globe"), "World Map"),
+    uiOutput("worldConfirmedDateSelector"),
     echarts4rOutput("worldConfirmed", height = "600px") %>% withSpinner()
   )
 )
