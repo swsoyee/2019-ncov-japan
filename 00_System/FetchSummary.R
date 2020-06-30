822af44abc9a9b43cb11410f74863b4f98dc913e
swsoyee <swsoyee@gmail.com>
2020-06-30 18:52:15 +0900

    [update] 厚労省データ

--- 00_System/FetchSummary.R
+++ 00_System/FetchSummary.R
@@ -143,7 +143,8 @@
 #   "20200626" = "https://www.mhlw.go.jp/content/10906000/000644137.pdf",
 #   "20200627" = "https://www.mhlw.go.jp/content/10906000/000644324.pdf",
 #   "20200628" = "https://www.mhlw.go.jp/content/10906000/000644366.pdf",
-#   "20200629" = "https://www.mhlw.go.jp/content/10906000/000644581.pdf"
+#   "20200629" = "https://www.mhlw.go.jp/content/10906000/000644581.pdf",
+#   "20200630" = "https://www.mhlw.go.jp/content/10906000/000645008.pdf"
 # )
 # 
 # for (i in names(location)) {
