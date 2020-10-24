12e94230b18c52ac4f85be976c99b69474928b20
swsoyee <swsoyee@gmail.com>
2020-10-24 22:53:30 +0900

    [update] 厚労省、自治体データ

--- 00_System/FetchSummary.R
+++ 00_System/FetchSummary.R
@@ -259,7 +259,8 @@
 #   "20201020" = "https://www.mhlw.go.jp/content/10906000/000684985.pdf",
 #   "20201021" = "https://www.mhlw.go.jp/content/10906000/000685495.pdf",
 #   "20201022" = "https://www.mhlw.go.jp/content/10906000/000686102.pdf",
-#   "20201023" = "https://www.mhlw.go.jp/content/10906000/000686678.pdf"
+#   "20201023" = "https://www.mhlw.go.jp/content/10906000/000686678.pdf",
+#   "20201024" = "https://www.mhlw.go.jp/content/10906000/000687050.pdf"
 # )
 # 
 # for (i in names(location)) {
