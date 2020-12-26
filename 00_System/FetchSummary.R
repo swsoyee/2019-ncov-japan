8d94fc10603e281ae44eda94d61e6b2487cecb9c
swsoyee <swsoyee@gmail.com>
2020-12-26 21:48:51 +0900

    [fix] 厚労省発表した確認中のデータ集計の自動集計を簡潔にする

--- 00_System/FetchSummary.R
+++ 00_System/FetchSummary.R
@@ -20,15 +20,25 @@
 #     colnames(dt) <- c("都道府県名", "陽性者", "検査人数", "入院中", "重症者", "退院者", "死亡者")
 #     dt <- dt[3:(nrow(dt) - 1)]
 #     dt[grepl("その他", 都道府県名), 都道府県名 := "伊客船"]
+#     # remove space
 #     dt[, 都道府県名 := gsub(" ", "", 都道府県名)]
+#     # remove notes
+#     dt[, 都道府県名 := gsub(pattern = "※\\d", "", 都道府県名)]
 #     cols <- colnames(dt)[2:ncol(dt)]
 #     dt[, (cols) := lapply(.SD, function(x){return(gsub(",", "", x))}), .SDcols = cols]
 #     suppressWarnings(dt[, (cols) := lapply(.SD, as.numeric), .SDcols = cols])
 #     dt$temp <- rowSums(dt[, c(4, 6:ncol(dt)), with = F], na.rm = T)
 #     dt[, 確認中 := 陽性者 - temp]
+#     # 確認中はマイナスの場合、退院者から引く
+#     dt[確認中 < 0, 退院者 := 退院者 + 確認中]
+#     dt[確認中 < 0, 確認中 := 0]
 #     dt <- cbind(data.table("日付" = rep(names(location[i]), nrow(dt))), dt, "分類" = 0)
 #     dt[, temp := NULL]
+#     # 空港、チャーター便、クルーズ船のデータ処理
+#     other <- tail(dataset, n = 3)
+#     suppressWarnings(other[, 日付 := as.character(names(location[i]))])
 #     dataset <- suppressWarnings(gtools::smartbind(dataset, dt))
+#     dataset <- rbind(dataset, other)
 #   }
 # }
 # 
