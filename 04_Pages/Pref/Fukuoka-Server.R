8c26b671ed1d407a1315b5ab1f9c84b90f480a3a
InfinityLoop <swsoyee@gmail.com>
2021-01-24 16:47:33 +0900

    fix: fukuoka UI and server

--- 04_Pages/Pref/Fukuoka-Server.R
+++ 04_Pages/Pref/Fukuoka-Server.R
@@ -39,7 +39,7 @@ observeEvent(input$sideBarTab, {
 
 output$FukuokaValueBoxes <- renderUI({
   totalPCR <- sum(GLOBAL_VALUE$Fukuoka$test$件数)
-  positiveDt <- GLOBAL_VALUE$Fukuoka$patients[, (計 = .N), by = "公表_年月日"]
+  positiveDt <- GLOBAL_VALUE$Fukuoka$patients[, (計 <- .N), by = "公表_年月日"]
   totalPositive <- sum(positiveDt$V1)
   discharge <- mhlwSummary[都道府県名 == "福岡"]
   discharge[, 日次退院者 := 退院者 - shift(退院者)]
@@ -48,56 +48,61 @@ output$FukuokaValueBoxes <- renderUI({
 
   dischargeRate <- paste0(
     round(
-      totalDischarge / (totalPositive - totalDeath)  * 100, 2
-    ), '%'
+      totalDischarge / (totalPositive - totalDeath) * 100, 2
+    ), "%"
   )
   deathRate <- paste0(
     round(
       totalDeath / totalPositive * 100, 2
-    ), '%'
+    ), "%"
   )
-  
+
   return(
     tagList(
       fluidRow(
-        createValueBox(value = totalPCR, # TODO 今はけんものデータを使ってる
-                       subValue = tagList(
-                         tags$span(id = "fukuokaTest", icon("info-circle"))
-                         ), 
-                       sparkline = createSparklineInValueBox(GLOBAL_VALUE$Fukuoka$test, '件数'),
-                       subtitle = i18n$t("検査数"),
-                       icon = 'vials',
-                       color = 'yellow', 
-                       diff = tail(GLOBAL_VALUE$Fukuoka$test$件数, n = 1)
+        createValueBox(
+          value = totalPCR, # TODO 今はけんものデータを使ってる
+          subValue = tagList(
+            tags$span(id = "fukuokaTest", icon("info-circle"))
+          ),
+          sparkline = createSparklineInValueBox(GLOBAL_VALUE$Fukuoka$test, "件数"),
+          subtitle = i18n$t("検査数"),
+          icon = "vials",
+          color = "yellow",
+          diff = tail(GLOBAL_VALUE$Fukuoka$test$件数, n = 1)
         ),
         bsTooltip(id = "fukuokaTest", "民間検査実施分が含まれていません。"),
-        createValueBox(value = totalPositive,
-                       subValue = paste0(i18n$t('速報：'), sum(byDate$福岡, na.rm = T)), 
-                       sparkline = createSparklineInValueBox(
-                         positiveDt, 
-                         'V1'),
-                       subtitle = i18n$t("陽性者数"),
-                       icon = 'procedures',
-                       color = 'red', 
-                       diff = tail(positiveDt$V1, n = 1)
+        createValueBox(
+          value = totalPositive,
+          subValue = paste0(i18n$t("速報："), sum(byDate$福岡, na.rm = T)),
+          sparkline = createSparklineInValueBox(
+            positiveDt,
+            "V1"
+          ),
+          subtitle = i18n$t("陽性者数"),
+          icon = "procedures",
+          color = "red",
+          diff = tail(positiveDt$V1, n = 1)
         )
       ),
       fluidRow(
-        createValueBox(value = totalDischarge, # TODO 今は厚労省のデータを使ってる
-                       subValue = dischargeRate, # TODO
-                       sparkline = createSparklineInValueBox(discharge, '日次退院者'),
-                       subtitle = i18n$t("回復者数"),
-                       icon = 'user-shield',
-                       color = 'green',
-                       diff = tail(discharge$日次退院者, n = 1)
+        createValueBox(
+          value = totalDischarge, # TODO 今は厚労省のデータを使ってる
+          subValue = dischargeRate, # TODO
+          sparkline = createSparklineInValueBox(discharge, "日次退院者"),
+          subtitle = i18n$t("回復者数"),
+          icon = "user-shield",
+          color = "green",
+          diff = tail(discharge$日次退院者, n = 1)
         ),
-        createValueBox(value = totalDeath, # TODO 公式データまだない
-                       subValue = deathRate,
-                       sparkline = createSparklineInValueBox(death, '福岡'),
-                       subtitle = i18n$t("死亡者数"),
-                       icon = 'bible',
-                       color = 'navy',
-                       diff = tail(death$福岡, n = 1)
+        createValueBox(
+          value = totalDeath, # TODO 公式データまだない
+          subValue = deathRate,
+          sparkline = createSparklineInValueBox(death, "福岡"),
+          subtitle = i18n$t("死亡者数"),
+          icon = "bible",
+          color = "navy",
+          diff = tail(death$福岡, n = 1)
         )
       )
     )
@@ -112,8 +117,10 @@ output$FukuokaInfectedRoute <- renderEcharts4r({
   }),
   .SD = c("感染経路不明", "濃厚接触者", "海外渡航歴有"), by = "公表_年月日"
   ]
-  dt[, `:=` (公表_年月日 = as.Date(公表_年月日), 
-                   累計 = cumsum(感染経路不明 + 濃厚接触者 + 海外渡航歴有))] %>%
+  dt[, `:=`(
+    公表_年月日 = as.Date(公表_年月日),
+    累計 = cumsum(感染経路不明 + 濃厚接触者 + 海外渡航歴有)
+  )] %>%
     e_chart(公表_年月日) %>%
     e_bar(感染経路不明, stack = 1, itemStyle = list(color = "#E9546B")) %>%
     e_bar(濃厚接触者, stack = 1, itemStyle = list(color = "#025BAC")) %>%
@@ -283,13 +290,15 @@ output$FukuokaCluster <- renderEcharts4r({
 output$fukuokaPatientTable <- renderDataTable({
   positiveDetail <- GLOBAL_VALUE$Fukuoka$nodes
   datatable(
-    positiveDetail[, .(番号 = 都道府県症例番号, 発症日 = as.Date(発症日), 公表日 = as.Date(公表日), 
-                         管轄 = (管理市区町村), 
-                         居住地 = (居住市区町村), 
-                         年代 = (年代), 
-                         性別 = (性別), 職業 = (職業))],
+    positiveDetail[, .(
+      番号 = 都道府県症例番号, 発症日 = as.Date(発症日), 公表日 = as.Date(公表日),
+      管轄 = (管理市区町村),
+      居住地 = (居住市区町村),
+      年代 = (年代),
+      性別 = (性別), 職業 = (職業)
+    )],
     # filter = 'top',
-    selection = 'single',
+    selection = "single",
     options = list(
       # filter = 'top'
     )
@@ -308,9 +317,9 @@ observeEvent(input$fukuokaPatientTable_rows_selected, {
 
 output$fukuokaProfile <- renderUI({
   selectedId <- input$fukuokaPatientTable_rows_selected
-  if(!is.null(selectedId)) {
+  if (!is.null(selectedId)) {
     profile <- GLOBAL_VALUE$Fukuoka$nodes[都道府県症例番号 == GLOBAL_VALUE$Fukuoka$nodes[selectedId]$都道府県症例番号]
-    
+
     age <- ifelse(profile$年代 != "", profile$年代, i18n$t("不明"))
     gender <- tagList(icon("venus-mars"), profile$性別)
     if (profile$性別 == "男性") {
@@ -318,7 +327,7 @@ output$fukuokaProfile <- renderUI({
     } else if (profile$性別 == "女性") {
       gender <- tagList(icon("venus"), i18n$t("女性"))
     }
-    
+
     # リンク分割
     outerLinks <- strsplit(profile$情報源, split = ";")[[1]]
     outerLinkTags <-
@@ -331,37 +340,35 @@ output$fukuokaProfile <- renderUI({
         )
       }))
 
-    boxPlus(
+    box(
       title = tagList(icon("id-card"), i18n$t("公開された感染者情報")),
       width = 12,
       closable = F,
       boxProfile(
         title = profile$都道府県症例番号,
-        src = ifelse(profile$性別 == '男性', 'Icon/male.png', 'Icon/female.png'),
+        src = ifelse(profile$性別 == "男性", "Icon/male.png", "Icon/female.png"),
         subtitle = tagList(gender),
-        boxProfileItemList(
-          bordered = TRUE,
-          boxProfileItem(
-            title = tagList(icon("user-clock"), i18n$t("年代")),
-            description = age
-          ),
-          boxProfileItem(
-            title = tagList(icon("bullhorn"), i18n$t("公表日")),
-            description = profile$公表日
-          ),
-          boxProfileItem(
-            title = tagList(icon("user-tie"), i18n$t("職業")),
-            description = profile$職業
-          ),
-          boxProfileItem(
-            title = tagList(icon("home"), i18n$t("居住地")),
-            description = paste(profile$居住都道府県, profile$居住市区町村)
-          ),
-          boxProfileItem(
-            title = tagList(icon("external-link-alt"), i18n$t("情報源")),
-            description = outerLinkTags
-          ),
-        )
+        bordered = TRUE,
+        boxProfileItem(
+          title = tagList(icon("user-clock"), i18n$t("年代")),
+          description = age
+        ),
+        boxProfileItem(
+          title = tagList(icon("bullhorn"), i18n$t("公表日")),
+          description = profile$公表日
+        ),
+        boxProfileItem(
+          title = tagList(icon("user-tie"), i18n$t("職業")),
+          description = profile$職業
+        ),
+        boxProfileItem(
+          title = tagList(icon("home"), i18n$t("居住地")),
+          description = paste(profile$居住都道府県, profile$居住市区町村)
+        ),
+        boxProfileItem(
+          title = tagList(icon("external-link-alt"), i18n$t("情報源")),
+          description = outerLinkTags
+        ),
       ),
       footer = tagList(
         tags$b(icon("handshake"), i18n$t("濃厚接触者状況")),
@@ -375,7 +382,7 @@ output$fukuokaProfile <- renderUI({
       )
     )
   } else {
-    boxPlus(
+    box(
       title = tagList(icon("id-card"), i18n$t("公開された感染者情報")),
       width = 12,
       closable = F,
@@ -388,22 +395,26 @@ output$fukuokaProfile <- renderUI({
 output$FukuokaContact <- renderEcharts4r({
   call <- GLOBAL_VALUE$Fukuoka$call
   # call[, `:=` (日付 = as.Date(日付))] # 20200717 から変更
-  call[, `:=` (日付 = as.Date(年月日), 合計 = 件数, 専用ダイヤル累計 = cumsum(件数))]
+  call[, `:=`(日付 = as.Date(年月日), 合計 = 件数, 専用ダイヤル累計 = cumsum(件数))]
   call %>%
     e_chart(日付) %>%
     e_bar(合計, itemStyle = list(color = lightBlue), name = i18n$t("新規")) %>%
     e_line(専用ダイヤル累計, name = i18n$t("累計"), itemStyle = list(color = darkNavy), y_index = 1, symbolSize = 1) %>%
-    e_title(text = "帰国者・接触者相談センター相談件数", 
-            subtext = paste0(
-              sprintf("最終更新日：%s", max(call$日付)), "   ",
-              sprintf("計：%s件", tail(call$専用ダイヤル累計, n = 1))
-            )) %>%
+    e_title(
+      text = "帰国者・接触者相談センター相談件数",
+      subtext = paste0(
+        sprintf("最終更新日：%s", max(call$日付)), "   ",
+        sprintf("計：%s件", tail(call$専用ダイヤル累計, n = 1))
+      )
+    ) %>%
     e_y_axis(splitLine = list(show = F), index = 1) %>%
-    e_y_axis(axisLabel = list(inside = T),
-             axisTick = list(show = F),
-             splitLine = list(lineStyle = list(opacity = 0.2))) %>%
+    e_y_axis(
+      axisLabel = list(inside = T),
+      axisTick = list(show = F),
+      splitLine = list(lineStyle = list(opacity = 0.2))
+    ) %>%
     e_x_axis(splitLine = list(lineStyle = list(opacity = 0.2))) %>%
-    e_grid(left = '5%', right = '10%') %>%
+    e_grid(left = "5%", right = "10%") %>%
     e_legend(
       type = "scroll",
       orient = "vertical",
@@ -418,7 +429,7 @@ output$FukuokaContact <- renderEcharts4r({
 # 検査実施数 ====
 output$FukuokaTest <- renderEcharts4r({
   test <- GLOBAL_VALUE$Fukuoka$test
-  test[, `:=` (年月日 = as.Date(年月日), 累計 = cumsum(件数))]
+  test[, `:=`(年月日 = as.Date(年月日), 累計 = cumsum(件数))]
   setnafill(test, fill = 0, cols = c("福岡市", "北九州市", "福岡県"))
   test %>%
     e_chart(年月日) %>%
@@ -427,11 +438,13 @@ output$FukuokaTest <- renderEcharts4r({
     e_bar(福岡県, stack = 1, itemStyle = list(color = lightYellow)) %>%
     e_line(累計, stack = 1, itemStyle = list(color = darkYellow), symbolSize = 1, y_index = 1) %>%
     e_y_axis(splitLine = list(show = F), index = 1) %>%
-    e_y_axis(axisLabel = list(inside = T),
-             axisTick = list(show = F),
-             splitLine = list(lineStyle = list(opacity = 0.2))) %>%
+    e_y_axis(
+      axisLabel = list(inside = T),
+      axisTick = list(show = F),
+      splitLine = list(lineStyle = list(opacity = 0.2))
+    ) %>%
     e_x_axis(splitLine = list(lineStyle = list(opacity = 0.2))) %>%
-    e_grid(left = '5%', right = '10%', top = "20%") %>%
+    e_grid(left = "5%", right = "10%", top = "20%") %>%
     e_legend(
       type = "scroll",
       orient = "vertical",
@@ -440,13 +453,15 @@ output$FukuokaTest <- renderEcharts4r({
       right = "15%"
     ) %>%
     e_tooltip(trigger = "axis") %>%
-    e_title(text = "検査実施数", 
-            subtext = paste0(
-              sprintf("最終更新日：%s", max(test$年月日)), "   ",
-              sprintf("計：%s件", sum(test$件数)), 
-              "\n※ 福岡県は福岡市、北九州市以外の自治体の合計です。",
-              "\n※ 民間検査実施分が含まれていません。"
-            )) %>%
+    e_title(
+      text = "検査実施数",
+      subtext = paste0(
+        sprintf("最終更新日：%s", max(test$年月日)), "   ",
+        sprintf("計：%s件", sum(test$件数)),
+        "\n※ 福岡県は福岡市、北九州市以外の自治体の合計です。",
+        "\n※ 民間検査実施分が含まれていません。"
+      )
+    ) %>%
     e_group("fukuokaBar") %>%
     e_connect_group("fukuokaBar")
 })
