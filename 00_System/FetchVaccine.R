72f065f922eacc2fc3f5a8b9e113477e7fb59b96
InfinityLoop <swsoyee@gmail.com>
2021-04-29 17:05:04 +0900

    feat: featch vaccine data by prefecture

---
+++ 00_System/FetchVaccine.R
@@ -0,0 +1,66 @@
+library(data.table)
+library(tabulizer)
+
+# Define data path
+data_path <- "50_Data/MHLW/vaccineByRegion.csv"
+
+# Read exist data
+vaccineByRegion <- fread(data_path)
+
+# Define origion
+definition <- list(
+  list(
+    category = "medical",
+    url = "https://www.kantei.go.jp/jp/content/IRYO-kenbetsu-vaccination_data.pdf"
+  ),
+  list(
+    category = "elderly",
+    url = "https://www.kantei.go.jp/jp/content/KOREI-kenbetsu-vaccination_data.pdf"
+  )
+)
+
+for (item in definition) {
+  # Extract update date in PDF file
+  date <- strsplit(tabulizer::extract_text(item$url), split = "\n")[[1]][2]
+  date <- paste0(format(Sys.Date(), "%Y"), "年", gsub("（(.*)時点）", "\\1", date))
+  date_new <- format(as.Date(date, format = "%Y年%m月%d日"), "%Y%m%d")
+
+  # Remove same data if existed (for update exist data)
+  vaccineByRegion <- vaccineByRegion[!(category == item$category & date == date_new)]
+
+  # Extract table from PDF
+  data <- tabulizer::extract_tables(item$url)
+  data <- data.table(data[[1]])[3:.N, ]
+  if (item$category == "elderly") {
+    pre_data <- data[, .(
+      prefecture = V1,
+      total = V3,
+      first = V5,
+      second = V7,
+      category = item$category,
+      date = date_new
+    )]
+  }
+  if (item$category == "medical") {
+    pre_data <- data[, .(
+      prefecture = V1,
+      total = V2,
+      first = V3,
+      second = V4,
+      category = item$category,
+      date = date_new
+    )]
+  }
+
+  pre_data[, c("code", "prefecture") := tstrsplit(prefecture, " ", fixed = TRUE)]
+
+  cols <- c("total", "first", "second")
+  pre_data[, (cols) := lapply(.SD, function(x) {
+    return(gsub(",", "", x))
+  }), .SDcols = cols]
+  suppressWarnings(pre_data[, (cols) := lapply(.SD, as.numeric), .SDcols = cols])
+
+  vaccineByRegion <- rbind(vaccineByRegion, pre_data)
+}
+
+fwrite(vaccineByRegion, file = data_path)
