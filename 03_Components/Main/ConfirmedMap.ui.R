38bcd0c233e0c590faf4af5b2e9a42bc0ef20348
InfinityLoop <swsoyee@gmail.com>
2021-05-22 16:01:42 +0900

    fix: description in vaccine ratio

--- 03_Components/Main/ConfirmedMap.ui.R
+++ 03_Components/Main/ConfirmedMap.ui.R
@@ -60,10 +60,10 @@ tabPanel(
       ),
       helpText(
         sprintf(
-          i18n$t("2021/2/17 接種開始日から %s 日を経ち、２回目接種完了率は %s。直近７日の平均毎日の接種完了数は %s、あと約 %s 日で目標の %s に達成できる。"),
+          i18n$t("2021/2/17 接種開始日から %s 日を経ち、２回目接種完了率は %s。直近７日の平均毎日の接種完了人数は %s、あと約 %s 日で目標の %s に達成できる。"),
           as.numeric(Sys.Date() - as.Date("2021-02-17")),
           paste0(round(global_value_for_display[key == "vaccine_complete"]$value / sum(prefecture_master$人口) * 100, 2), "%"),
-          global_value_for_display[key == "average_7_vaccine"]$value,
+          prettyNum(global_value_for_display[key == "average_7_vaccine"]$value, big.mark = ","),
           # sum(prefecture_master$人口) * 0.6
           round((75729685 - global_value_for_display[key == "vaccine_complete"]$value) / global_value_for_display[key == "average_7_vaccine"]$value),
           "60 %"
